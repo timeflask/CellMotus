2a54b2d68f5c56da40fc45a644cb662f04df75cf
TimeFlask <timeflask@gmail.com>
2014-12-19 20:19:28 -0800

    android+linux releases

--- src/linux/main.c
+++ src/linux/main.c
@@ -1,6 +1,13 @@
 #include "sen.h"
 #include "x_glfw_desktop_app.h"
+#ifdef SEN_DEBUG
 int main() {
 //  sen_assets_set_root("../");
   return sen_desktop_app_run();
 }
+#else
+int main() {
+  sen_assets_set_root("../");
+  return sen_desktop_app_run();
+}
+#endif
